874e8f39cbb872ca808c5b17a0db23980add303b
Helwiza Fahry <helwyza@gmail.com>
2026-01-09 10:04:42 +0700

    new endpoint

--- main.go
+++ main.go
@@ -14,9 +14,15 @@ func statusHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Status: OK")
 }
 
+func newEndpoint(w http.ResponseWriter, r *http.Request) {
+	// New functionality can be added here
+	fmt.Fprintln(w, "New Endpoint Reached")
+}
+
 func main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/status",statusHandler )
+	http.HandleFunc("/new", newEndpoint)
 	log.Println("Server running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
