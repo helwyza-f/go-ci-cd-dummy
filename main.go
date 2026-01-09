7a23ada594d1bf3e5e7ee44a475896d43ddf6d3d
Helwiza Fahry <helwyza@gmail.com>
2026-01-09 09:19:24 +0700

    retest git push to trigger github actions

--- main.go
+++ main.go
@@ -10,8 +10,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "CI/CD Go backend WORKS 🚀")
 }
 
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Status: OK")
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/status",statusHandler )
 	log.Println("Server running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
